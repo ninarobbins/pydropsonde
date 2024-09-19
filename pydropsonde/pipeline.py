a544e5f78a31cdd91104cf156187c21cea91ce81
Nina Robbins Blanch <nina.robbins@mpimet.mpg.de>
2024-09-19 13:28:46 -0400

    apply fitting to variable list

--- pydropsonde/pipeline.py
+++ pydropsonde/pipeline.py
@@ -563,7 +563,11 @@ pipeline = {
     "calculate_circle_data": {
         "intake": "circles",
         "apply": iterate_Circle_method_over_dict_of_Circle_objects,
-        "functions": ["dummy_circle_function", "get_xy_coords_for_circles"],
+        "functions": [
+            "dummy_circle_function",
+            "get_xy_coords_for_circles",
+            "fit_multiple_vars",
+        ],
         "output": "circles",
         "comment": "calculate circle products",
     },
