a8624a789006af0d7b797751b28de15edf4b3e6e
Nina Robbins Blanch <nina.robbins@mpimet.mpg.de>
2024-09-23 14:19:37 -0400

    add divergence and vorticity

--- pydropsonde/circles.py
+++ pydropsonde/circles.py
@@ -18,3 +18,12 @@ class Circle:
     def dummy_circle_function(self):
         print(self.flight_id, self.segment_id)
         return self
+
+    def get_div_and_vor(self):
+        D = self.circle_ds.dudx + self.circle_ds.dvdy
+        vor = self.circle_ds.dvdx - self.circle_ds.dudy
+
+        self.circle_ds = self.circle_ds.assign(
+            dict(div=(["alt"], D.values), vor=(["alt"], vor.values))
+        )
+        return self
