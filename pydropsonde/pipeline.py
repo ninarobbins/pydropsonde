0956390a10d7d0f405d6a5fdb387a1e7d84f0234
Nina Robbins Blanch <nina.robbins@mpimet.mpg.de>
2024-12-20 14:59:22 +0100

    adjust pipeline for ragged dataset

--- pydropsonde/pipeline.py
+++ pydropsonde/pipeline.py
@@ -382,7 +382,7 @@ def sondes_to_gridded(sondes: dict, config: configparser.ConfigParser):
 def circles_to_gridded(circles: dict, config: configparser.ConfigParser):
     global_attrs = get_global_attrs_from_config(config)
     gridded = Gridded(circles=circles, sondes=None, global_attrs=global_attrs)
-    gridded.concat_circles()
+    gridded.concatenate_circles_with_ragged_structure()
     return gridded
 
 
@@ -631,7 +631,12 @@ pipeline = {
     "create_L4": {
         "intake": "gridded",
         "apply": apply_method_to_dataset,
-        "functions": ["concat_circles", "get_l4_dir", "get_l4_filename", "write_l4"],
+        "functions": [
+            "concatenate_circles_with_ragged_structure",
+            "get_l4_dir",
+            "get_l4_filename",
+            "write_l4",
+        ],
         "output": "gridded",
         "comment": "This step concatenates circles and creates the L4 dataset after adding additional products.",
     },
